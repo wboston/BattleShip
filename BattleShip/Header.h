b0f1fbdab34f61374a4f9ac448b0f9386028d66a
William <williamboston123@hotmail.com>
2018-09-27 19:11:36 -0700

    Add: File Structure and Library includes

--- BattleShip/Header.h
+++ BattleShip/Header.h
@@ -1,1 +1,16 @@
 #pragma once
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+
+
+
+#endif // !HEADER_H
